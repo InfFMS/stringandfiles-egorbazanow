8378e374f5d103a1f340df42e7640f4bd81c83ef
Егор Базанов <egorbazanow@bk.ru>
2025-01-20 14:08:50 +0700

    done 2

--- task1.py
+++ task1.py
@@ -8,12 +8,11 @@ with open("task1.txt", "r", encoding="utf-8") as f:
    line = f.readlines()
 line_n = len(line)
 word_n = 0
-for line in line:
-    words = line.split()
-    word_n += len(words)
 symbol_nums = 0
-for line in line:
-    symbol_nums += len(line)
+for abc in line:
+    words = abc.split()
+    word_n += len(words)
+    symbol_nums += len(abc)
 print("Общее количество строк:", line_n)
-print("Общее количество слов:", word_n)
+print("Общее количество слов:", word_n - 1)
 print("Общее количество символов (включая пробелы):", symbol_nums)
